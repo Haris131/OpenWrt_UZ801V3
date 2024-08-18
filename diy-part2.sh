bb49bf8b7604e200810a582025bbc8b011a60bcf
Haris131 <41672454+Haris131@users.noreply.github.com>
2024-08-18 16:53:39 +0700

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,6 +16,9 @@ sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_gener
 # Change hash kernel
 sed -i 's/07237fd9f66ce4b95d551569e2f8395342fdb720305f01aa52cd5ca216594278/1b6b3bded4c81814ebebe2d194c2f8966d2399005b85ebb0557285b6e73f5422/g' package/kernel/mac80211/Makefile
 
+# Modify default cpu sub type
+sed -i 's/CPU_SUBTYPE:=neon//g' target/linux/msm89xx/msm8916/target.mk
+
 # Clone community packages to package
 mkdir -p package/community
 pushd package/community
