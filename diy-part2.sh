5974bb777f61ab950eead1a4f6abf7260fdf715e
Haris131 <41672454+Haris131@users.noreply.github.com>
2024-08-26 19:21:21 +0700

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,15 +13,6 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
 
-# Update kernel version
-sed -i 's/LINUX_VERSION-5.10 = .33/LINUX_VERSION-6.6 = .47/g' include/kernel-version.mk
-sed -i 's/LINUX_KERNEL_HASH-5.10.33 = 933fdbc36371c0f830b7a6a957a559fca2dad1cc0eaa852ef42fb168185b4315/LINUX_KERNEL_HASH-6.6.47 = d43376c9e9eaa92bb1b926054bd160d329c58a62d64bd65fe1222c11c6564f50/g' include/kernel-version.mk
-sed -i 's/CONFIG_LINUX_5_10=y/CONFIG_LINUX_6_6=y/g' .config
-sed -i 's/KERNEL_PATCHVER:=5.10/KERNEL_PATCHVER:=6.6/g' target/linux/msm89xx/Makefile
-sed -i 's/KERNEL_TESTING_PATCHVER:=5.10/KERNEL_TESTING_PATCHVER:=6.6/g' target/linux/msm89xx/Makefile
-rm -rf target/linux/msm89xx/patches-5.10
-mv target/linux/msm89xx/config-5.10 target/linux/msm89xx/config-6.6
-
 # Update hash kernel
 sed -i 's/07237fd9f66ce4b95d551569e2f8395342fdb720305f01aa52cd5ca216594278/1b6b3bded4c81814ebebe2d194c2f8966d2399005b85ebb0557285b6e73f5422/g' package/kernel/mac80211/Makefile
 sed -i 's/CPU_SUBTYPE:=neon//g' target/linux/msm89xx/msm8916/target.mk
